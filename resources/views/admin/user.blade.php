bbbff3c93b9917197ef5822c07492c31e8065fd2
mohlutfifadilah <mohlutfifadilah23@gmail.com>
2021-11-23 22:23:36 +0700

    upload project pertama

---
+++ resources/views/admin/user.blade.php
@@ -0,0 +1,21 @@
+@extends('admin.layouts')
+@section('title','User')
+@section('content')
+<!-- Main Content -->
+<div class="main-content">
+    <section class="section">
+      <div class="section-header">
+        <h1>User</h1>
+      </div>
+      <div class="section-body">
+          <table id="myTable">
+              <tr>
+                  <td>dwa</td>
+                  <td>daw</td>
+                  <td>dwa</td>
+              </tr>
+          </table>
+      </div>
+    </section>
+  </div>
+@endsection
